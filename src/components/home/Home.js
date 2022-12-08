1852cf183b3bd8e603e783c7f84b31b33c5d6913
Ysquid <ahk648@gmail.com>
2022-12-08 16:10:44 -0600

    fleshing out about with interest list

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -16,10 +16,18 @@ function Home() {
           spare time. In September of 2022 I began dedicating myself full-time
           to learning full-stack web development. My ultimate goal is to become
           a full-time professional developer. I am open to in-person roles
-          across Canada as well as remote positions. In my spare time I like to
-          golf, run, watch football, and play video games. I drink entirely too
-          much coffee and love dogs.
+          across Canada as well as remote positions.
         </p>
+        <br></br>
+        
+        <ul aria-label="Interests">
+          <li>Technology - especially web development & data engineering and visualization</li>
+          <li>Sports - especially football - go Wolverines - go Bengals!</li>
+          <li>Gaming - any TFT players hit me up!</li>
+          <li>Fitness - sometimes this one is more of a job than interest</li>
+          <li>Food - cooking it, eating it, learning about it</li>
+          <li>Music - there is nothing more I love than a good concert</li>
+        </ul>
       </div>
       <div className="Picture">
         <img src={Me} />
