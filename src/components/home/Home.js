ff8aa71cff8b22741845ec820c9d42e9d29f91aa
Ysquid <ahk648@gmail.com>
2022-12-08 15:51:43 -0600

    changed layout swap to 850px. Adding about content

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
+import Me from '../../assets/me.jpg'
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="Home">
+      <div className="About">
+        <h1>About Me</h1>
+        <p>
+          Hi, I'm Ahmad. I am a 100% self taught full stack developer from
+          Saskatoon Sasktchewan. After several years of professional experience
+          working in areas such as sales, marketing, and data analysis, I
+          decided to pursue a field that has always interested me - computer
+          programming. In March of 2022 I started learning programming in my
+          spare time. In September of 2022 I began dedicating myself full-time
+          to learning full-stack web development. My ultimate goal is to become
+          a full-time professional developer. I am open to in-person roles
+          across Canada as well as remote positions. In my spare time I like to
+          golf, run, watch football, and play video games. I drink entirely too
+          much coffee and love dogs.
+        </p>
+      </div>
+      <div className="Picture">
+        <img src={Me} />
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
