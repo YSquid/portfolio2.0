28e7f1062c89b79e49340f8575bfeec5db576376
Ysquid <ahk648@gmail.com>
2022-12-09 13:38:00 -0600

    build projects object. add preview images

---
+++ src/components/projects/projectsList/projectsStorage.js
@@ -0,0 +1,29 @@
+const projects = {
+  portfolioSite: {
+    title: "This Website",
+    tech: ["React", "HTML5", "CSS3", "Git/GitHub"],
+    links: {
+      website: "http://localhost:3000/",
+      github: "https://github.com/YSquid/portfolio2.0",
+    },
+    previewURL: "../../assets/portfolio.png",
+  },
+  redditMininmal: {
+    title: "Reddit Clone",
+    tech: ["React", "Redux", "HTML5", "CSS3", 'Git/GitHub'],
+    links: {
+      website: "https://ak-reddit-minimal.netlify.app/",
+      github: "https://github.com/YSquid/reddit-minimal",
+    },
+    previewURL: "../../assets/redditClient.png",
+  },
+  3: {
+    mrkCBD: "Mr. K CBD Landing Page",
+    tech: ["HTML5", "CSS3", "Git/GitHub"],
+    links: {
+      website: "https://mrkcbd.com/",
+      github: "https://github.com/YSquid/mrk-landing",
+    },
+    previewURL: "../../assets/mrkCBD.jpg",
+  }
+};
