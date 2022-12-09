5ae82bbe8a595b4a91d27d98981ff8a6a2053791
Ysquid <ahk648@gmail.com>
2022-12-09 15:00:04 -0600

    more styling on projects page

--- src/components/projects/project/Project.js
+++ src/components/projects/project/Project.js
@@ -1,27 +1,30 @@
 import React from "react";
-import './Project.css'
+import "./Project.css";
 
 function Project({ id, title, tech, links, previewURL }) {
   return (
     <div className="Project">
-      <h2 className="Project_title">{title}</h2>
-      <ul className="Tech_list">
-        {tech.map((tech) => {
-          return <li key={tech}>{tech}</li>;
-        })}
-      </ul>
-      <ul className="Links">
-        <li>
-          <a href={links.website} target="_blank" rel="noreferrer">
-            View Site
-          </a>
-        </li>
-        <li>
-          <a href={links.github}>GitHub Repo</a>
-        </li>
-      </ul>
-
-      <img className="Preview_image" src={previewURL}/>
+      <div className="Project_left">
+        <h2 className="Project_title">{title}</h2>
+        <ul className="Tech_list">
+          {tech.map((tech) => {
+            return <li key={tech}>{tech}</li>;
+          })}
+        </ul>
+        <ul className="Links">
+          <li>
+            <a href={links.website} target="_blank" rel="noreferrer">
+              View Site
+            </a>
+          </li>
+          <li>
+            <a href={links.github} target="_blank" rel="noreferrer">GitHub Repo</a>
+          </li>
+        </ul>
+      </div>
+      <div className="Project_right">
+      <img className="Preview_image" src={previewURL} />
+      </div>
     </div>
   );
 }
