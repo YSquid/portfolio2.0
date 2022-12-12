ff62e15ae166ad17be5e47c45970c4c1f9dbdf3d
Ysquid <ahk648@gmail.com>
2022-12-12 10:29:44 -0600

    started skills section

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import "./Skills.css";
 
 function Skills() {
   return (
-    <div>Skills</div>
-  )
+    <section className="Skills">
+      <div className="Skills_container">
+        <div className="Technical_skills">
+          <h2>Technical Skills</h2>
+        </div>
+        <div className="Soft_skills">
+          <h2>Soft Skills</h2>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Skills
+export default Skills;
